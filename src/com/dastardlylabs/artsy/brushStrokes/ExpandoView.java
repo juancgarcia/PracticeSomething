9623adcd08ccbbd89df5cf9348add92a06ad3d0e
Juan Garcia <juangarcia.info@gmail.com>
2012-04-18 19:29:30 -0400

    Dev branch for work on custom View menus and experimenting with playback

---
+++ src/com/dastardlylabs/artsy/brushStrokes/ExpandoView.java
@@ -0,0 +1,26 @@
+package com.dastardlylabs.artsy.brushStrokes;
+
+import android.content.Context;
+import android.view.animation.Animation;
+import android.widget.LinearLayout;
+
+public class ExpandoView extends LinearLayout {
+	
+	Animation expand, contract;
+
+	public ExpandoView(Context context) {
+		super(context);
+		
+		//expand = new ScaleAnimation()
+
+	}
+	
+	public void grow(){
+		
+	}
+	
+	public void shrink(){
+		
+	}
+
+}
